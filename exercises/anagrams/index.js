0f636802026457523fc5d541112b5a44a64423b4
github username <github email address>
2019-07-13 11:59:05 -0700

    anagrams complete

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,45 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+let stringA = "A tree, a life, a bench";
+let stringB = "A tree, a fence, a yard";
+
+function anagrams(stringA, stringB) {
+  let revisedA = stringA.replace(/\W/g, "").toLowerCase();
+  let revisedB = stringB.replace(/\W/g, "").toLowerCase();
+  console.log(revisedA);
+  console.log(revisedB);
+
+  if (revisedA.length !== revisedB.length) {
+    return false;
+  }
+  let objA = {};
+  let objB = {};
+  for (let i = 0; i < revisedA.length; i++) {
+    if (objA[revisedA[i]]) {
+      objA[revisedA[i]] += 1;
+    } else {
+      objA[revisedA[i]] = 1;
+    }
+  }
+  console.log(objA);
+
+  for (let j = 0; j < revisedB.length; j++) {
+    if (objB[revisedB[j]]) {
+      objB[revisedB[j]] += 1;
+    } else {
+      objB[revisedB[j]] = 1;
+    }
+  }
+
+  for (key in objA) {
+    if (objA[key] !== objB[key] || !objB[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(anagrams(stringA, stringB));
 
 module.exports = anagrams;
