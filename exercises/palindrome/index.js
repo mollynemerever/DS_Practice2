d18b47fa4f06e944f115c3c22bf3429d2ddadd0c
github username <github email address>
2019-07-17 15:27:46 -0700

    palindrome complete

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,12 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+  let reversed = str
+    .split("")
+    .reverse()
+    .join("");
+  return reversed === str;
+}
 
 module.exports = palindrome;
