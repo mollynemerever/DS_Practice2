8171c3e0d2c4e214e8b6215b9455b99fc172e676
github username <github email address>
2019-07-18 11:37:56 -0700

    reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,15 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let string = n.toString();
+  let array = string.split("");
+  let reversedArray = array.reverse();
+  if (reversedArray[reversedArray.length - 1] === "-") {
+    let neg = reversedArray.pop();
+    reversedArray.unshift(neg);
+  }
+  return parseInt(reversedArray.join(""));
+}
 
 module.exports = reverseInt;
