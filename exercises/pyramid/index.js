c0fc8a4d3bc4fc3da19d4398b40bd458d162618c
github username <github email address>
2019-07-20 11:46:20 -0700

    pyramid - hard

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,21 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  let midpoint = Math.floor((n * 2 - 1) / 2);
+  let length = n * 2 - 1;
+
+  for (let row = 0; row < n; row++) {
+    let level = "";
+    for (let column = 0; column < length; column++) {
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += "#";
+      } else {
+        level += " ";
+      }
+    }
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
